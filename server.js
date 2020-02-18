4330ff923192f6339221576cbae44eb2bfe0244b
Tim Petrov <timofey1992@gmail.com>
2020-02-19 01:43:16 +0300

    added database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-slippery-16212',
-    user : 'spectral_wind',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
